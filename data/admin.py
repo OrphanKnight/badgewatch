97f5e6672d32adf79ffb41ce34abb93320d7d354
OrphanKnight <47511552+OrphanKnight@users.noreply.github.com>
2020-12-07 15:52:49 -0500

    added import/export feature to the admin

--- data/admin.py
+++ data/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from.models import Officer, Complaint
 
 # Register your models here.
-admin.site.register(Officer)
-admin.site.register(Complaint)
+class OfficerResource(resources.ModelResource):
+
+    class Meta:
+        model = Officer
+
+class OfficerAdmin(ImportExportModelAdmin):
+    resource_class = OfficerResource
+
+class ComplaintResource(resources.ModelResource):
+
+    class Meta:
+        model = Complaint
+
+class ComplaintAdmin(ImportExportModelAdmin):
+    resource_class = ComplaintResource
+
+
+admin.site.register(Officer, OfficerAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
