9ce82575497ee31e86d7b5677b06887b7f1f777f
OrphanKnight <47511552+OrphanKnight@users.noreply.github.com>
2020-12-07 14:59:36 -0500

    complaints table added

--- data/models.py
+++ data/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
 # Create your models here.
 
 class Officer(models.Model):
-    badgeID=models.CharField(max_length = 100)
-    firstName=models.CharField(max_length = 100)
-    lastName=models.CharField(max_length = 100)
+    badgeID=models.CharField(max_length = 20)
+    firstName=models.CharField(max_length = 20)
+    lastName=models.CharField(max_length = 20)
+
+class Complaint(models.Model):
+    categroy=models.CharField(max_length = 50)
+    badgeID=models.CharField(max_length = 20)
+    firstName=models.CharField(max_length = 20)
+    lastName=models.CharField(max_length = 20)
+    classification=models.CharField(max_length = 20)
+    allegation=models.CharField(max_length = 20)
+    finding=models.CharField(max_length = 20)
+    
+
+
 
